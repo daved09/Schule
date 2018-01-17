173b21667e7f893d3b1950915c67f6bb14b359ae
daved09 <daluba@live.de>
2018-01-17 08:40:51 +0100

    something changed

--- src/Girokonto/KontoMain.java
+++ src/Girokonto/KontoMain.java
@@ -2,7 +2,6 @@ package Girokonto;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 import static java.lang.System.out;
 
@@ -12,23 +11,19 @@ public class KontoMain {
     private Bank kaBank;
 
     public static void main(String[] args){
-        try{
             KontoMain main = new KontoMain();
             main.createKABank();
             main.doSomething();
-        }
-        catch (Exception e){
-            main(args);
-        }
     }
 
-    private void createKABank(){
+    private void createKABank() {
         kaBank = new Bank("Karlsruher Bank", "12345678");
         nummern.add(kaBank.neuesKonto("David Bannasch", 0.00));
         nummern.add(kaBank.neuesKonto("Deine Mudda", 3000));
         nummern.add(kaBank.neuesKonto("Markus Liebel", 0.00));
         out.println();
         kaBank.ueberweise(nummern.get(1), nummern.get(0), 1200);
+        kaBank.ueberweise(nummern.get(1), nummern.get(2), 400);
         kaBank.print();
         kaBank.loescheKonto(nummern.get(0));
     }
